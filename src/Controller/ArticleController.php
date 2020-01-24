0ad91562d4e3c2dfc04e079a38f371ff0e91c5e2
AnasErr <anaserrahmadi@gmail.com>
2020-01-24 15:51:57 +0100

    create new service

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,6 +3,7 @@
 
 namespace App\Controller;
 
+use App\Service\MarkdownHelper;
 use Michelf\MarkdownInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
@@ -24,7 +25,7 @@ class ArticleController extends AbstractController
      * @Route("/news/{slug}", name="article_show")
      * @return Response
      */
-    public function show($slug, MarkdownInterface $markdown, AdapterInterface $cache){
+    public function show($slug, MarkdownHelper $helper){
         $comments = [
             'I ate a normal rock once. It did NOT taste like bacon!',
             'Woohoo! I\'m going on an all-asteroid diet!',
@@ -49,13 +50,8 @@ cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim ca
 fugiat.
 EOF;
 
-        $item = $cache->getItem('markdown_'.md5($articleContent));
-        if(!$item->isHit())
-        {
-            $item->set($markdown->transform($articleContent));
-            $cache->save($item);
-        }
-        $articleContent = $item->get();
+
+        $articleContent = $helper->parse($articleContent);
         return $this->render('article/show.html.twig',[
             'title' => ucwords(str_replace('-',' ', $slug)),
             'articleContent' => $articleContent,
